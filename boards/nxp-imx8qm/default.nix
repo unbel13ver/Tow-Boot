bb9dcce4db2adc103387dac943c8e73d374b4f72
Ivan Nikolaenko <ivan.nikolaenko@unikie.com>
2022-08-17 13:42:07 +0300

    Get rid of imx-mkimage.
    Signed-off-by: Ivan Nikolaenko <ivan.nikolaenko@unikie.com>

--- boards/nxp-imx8qm/default.nix
+++ boards/nxp-imx8qm/default.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 
 let
-  mkimage = pkgs.pkgsBuildHost.callPackage ./imx-mkimage.nix {ubootpkgs = pkgs;};
+  imxOpteeOs = pkgs.callPackage ./imx-optee-os.nix {inherit pkgs;};
 in
 {
   device = {
@@ -16,7 +16,7 @@ in
 
   Tow-Boot =  {
 
-    uBootVersion = "2021.04";
+    uBootVersion = "2022.04";
 
     src = fetchGit {
         url = "https://source.codeaurora.org/external/imx/uboot-imx.git";
@@ -28,8 +28,17 @@ in
     useDefaultPatches = false; # until ported to 2022.04
 
     builder = {
+      postPatch = ''
+        install -m 0644 $BL31/bl31.bin ./
+        install -m 0644 $FWDIR/* ./
+        install -m 0644 ${imxOpteeOs}/tee.bin ./
+        echo "IMAGE A35 tee.bin 0xfe000000" >> board/freescale/imx8qm_mek/uboot-container.cfg
+      '';
+
+      makeFlags = [ "spl/u-boot-spl.bin" "flash.bin" "-j32"];
+
       installPhase = ''
-        install -m 0755 ${mkimage}/flash.bin $out/binaries/Tow-Boot.$variant.bin
+        install -m 0644 flash.bin $out/binaries/Tow-Boot.$variant.bin
       '';
     };
 
